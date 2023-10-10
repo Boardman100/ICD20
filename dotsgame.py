4d624b85afc4fff09491190bbb2acd83d8502a48
Stefano Edwards <sedwards@bayviewglen.ca>
2023-10-10 15:31:55 -0400

    initial commit

---
+++ dotsgame.py
@@ -0,0 +1,37 @@
+name = input("Input your name: ")
+opp = "Opponent"
+yp = "Your Points"
+op = "Opponent Points"
+box = "Box %"
+'''
+print("Game #1")
+op1 = input("Input the name of your first opponent: ")
+mp1 = int(input("Input the number of points that you scored: "))
+opp1 = int(input("Input the number of points that your first opponent scored: "))
+per1 = round(mp1/36*100,2)
+
+print("Game #2")
+op2 = input("Input the name of your second opponent: ")
+mp2 = int(input("Input the number of points that you scored: "))
+opp2 = int(input("Input the number of points that your second opponent scored: "))
+per2 = round(mp2/36*100,2)
+
+print("Game #3")
+op3 = input("Input the name of your third opponent: ")
+mp3 = int(input("Input the number of points that you scored: "))
+opp3 = int(input("Input the number of points that your third opponent scored: "))
+per3 = round(mp3/36*100,2)
+
+print("Game #4")
+op4 = input("Input the name of your fourth opponent: ")
+mp4 = int(input("Input the number of points that you scored: "))
+opp4 = int(input("Input the number of points that your fourth opponent scored: "))
+per4 = round(mp4/36*100,2)
+
+print("Game #5")
+op5 = input("Input the name of your fifth opponent: ")
+mp5 = int(input("Input the number of points that you scored: "))
+opp5 = int(input("Input the number of points that your fifth opponent scored: "))
+per5 = round(mp5/36*100,2)
+'''
+print(f"{opp:<15}{yp:<15}{op:<19}{box}")
